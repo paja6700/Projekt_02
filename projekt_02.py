8dbb9a437092952877615715eda69e2f1b167ab3
paja6700 <paja6700@gmail.com>
2024-12-15 19:33:58 +0100

    added game rules

---
+++ projekt_02.py
@@ -0,0 +1,24 @@
+"""
+projekt_2.py: druhy projekt do Engeto Online Python Akademie
+author: Pavel Křivan
+email: paja6700@gmail.com
+discord: pavel007111
+"""
+
+import sys
+
+#show game rules
+def show_rules():
+    print("""Welcome to Tic Tac Toe
+========================================
+GAME RULES:
+Each player can place one mark (or stone)
+per turn on the 3x3 grid. The WINNER is
+who succeeds in placing three of their
+marks in a:
+* horizontal,
+* vertical or
+* diagonal row
+========================================
+Let's start the game""")
+    
