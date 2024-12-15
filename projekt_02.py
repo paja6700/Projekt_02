2026263f970bdf3f3cbf6f4e9b0335198cd8b0ba
paja6700 <paja6700@gmail.com>
2024-12-15 19:46:06 +0100

    added function for check game board

--- projekt_02.py
+++ projekt_02.py
@@ -31,3 +31,17 @@ def display_board(board):
     print("+---+---+---+")
     print("========================================")
 
+#check a winner
+def check_winner(board):
+    # Check rows, columns, and diagonals
+    for row in board:
+        if row[0] == row[1] == row[2] and row[0] != " ":
+            return row[0]
+    for column in range(3):
+        if board[0][column] == board[1][column] == board[2][column] and board[0][column] != " ":
+            return board[0][column]
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != " ":
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != " ":
+        return board[0][2]
+    return None
